106c1eb933fb1746298c5fd5193077817965e462
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-05-26 18:22:11 +0300

    add user schema

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
